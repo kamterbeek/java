568a4869c1b3a90262e289c06f64c12f956b8192
Kassandra <kam.terbeek@gmail.com>
2025-04-21 23:10:46 -0600

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,24 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a greeting
+        System.out.println("Hello, Java world!");
+
+        // Declare and initialize a variable
+        int number = 5;
+        System.out.println("The number is: " + number);
+
+        // Conditional statement
+        if (number > 3) {
+            System.out.println("The number is greater than 3.");
+        } else {
+            System.out.println("The number is 3 or less.");
+        }
+
+        // Loop example
+        System.out.println("Counting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
