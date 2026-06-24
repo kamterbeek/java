49fb3412838d52c7f2f6d5258abbce8500995c9b
Kassandra <kam.terbeek@gmail.com>
2026-06-23 22:47:52 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -22,7 +22,6 @@ public class HelloWorld {
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Java world!");
 
             
             
